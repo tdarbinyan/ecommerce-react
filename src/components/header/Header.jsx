6433610ce76eb3768b5152bb1b1b85403f400499
vetlanova <vetlanova.maria@gmail.com>
2023-10-14 17:52:47 +0300

    new version

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,41 +1,27 @@
-import './header.css'
-import menuIcon from './../../img/menu_icon.svg' //временная мера, потом занесу импорт всех изображений в отдельный файл или подключу иконки
-import searchIcon from './../../img/search_icon.svg'
-import favIcon from './../../img/fav_icon.svg'
-import cartIcon from './../../img/cart_icon.svg'
-import profileIcon from './../../img/profile_icon.svg'
-function Header () {
-    return (
-        <header className="header">
-
-            <div className = "menu">
-                <button className = "menu_btn">
-                    <img src = {menuIcon} alt = "menuIcon"></img>
-                </button>
-            </div>
-            <button className = "logo" type = "submit">
-                logo
-            </button>
-            <div className = "search">
-                <input className = "search_line"></input>
-                <button className = "search_btn">
-                    <img src = {searchIcon} alt = "searchIcon"></img>
-                </button>
-            </div>
-            <button className='white_btn'>
-                <img src = {favIcon} alt = "menuIcon"></img>
-            </button>
-            <button className='white_btn'>
-                <img src = {cartIcon} alt = "menuIcon"></img>
-                {/* не забыть тут написать код */}
-                <div className='red_note'>1</div> 
-            </button>
-            <button className='white_btn'>
-                <img src = {profileIcon} alt = "menuIcon"></img>
-            </button>
-
-        </header>
-    )
+import "./header.css";
+import menuIcon from "./../../img/menu_icon.svg"; //временная мера, потом занесу импорт всех изображений в отдельный файл или подключу иконки
+import searchIcon from "./../../img/search_icon.svg";
+import favIcon from "./../../img/fav_icon.svg";
+import cartIcon from "./../../img/cart_icon.svg";
+import profileIcon from "./../../img/profile_icon.svg";
+function Header() {
+  return (
+    <header className="header">
+      <a href="#" className="menu"><img src = {menuIcon}></img></a>
+      <a href="#" className="header__logo"><h1>logo</h1></a>
+      <div className="form">
+        <input type="search" placeholder="Search models and collections" />
+        <button className="search__button"><img src = {searchIcon}></img></button>
+      </div>
+      <nav className="header__nav">
+        <ul className="header__list">
+          <li className="header__item" href="#"><img src = {favIcon}></img></li>
+          <li className="header__item" href="#"><img src = {cartIcon}></img></li>
+          <li className="header__item" href="#"><img src = {profileIcon}></img></li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
