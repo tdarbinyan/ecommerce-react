eed699f38b6096e8bc6c3622e617ea06234065d3
Telemak <darbinyantelemak@gmail.com>
2023-10-17 00:28:16 +0300

    commiting test version to test branch

--- src/main/Main.jsx
+++ src/main/Main.jsx
@@ -5,14 +5,14 @@ import nextIcon from "./../img/next_icon.svg";
 function Main() {
   return (
     <section className="main">
-      {/* <img src={mainPhoto} className="main_photo"></img> */}
+      <img src={mainPhoto} className="main_photo"></img>
 
-      {/* <div className="purple_form"> */}
+      <div className="purple_form">
         {/* <p>some text describing our bestseller</p> */}
         {/* <button className="purple_button"> */}
           {/* <h1>add to the cart</h1> */}
         {/* </button> */}
-      {/* </div> */}
+      </div>
 
       {/* <button className="main__next_button">
         <img src={nextIcon}></img>
