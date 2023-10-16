6d1a3a4563089b643fa0d906b21c184048f47d7f
vetlanova <vetlanova.maria@gmail.com>
2023-10-16 15:12:09 +0300

    i added background for main page, also i removed horizontal scrolling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Header from './components/header/Header.jsx'
-import Main from './components/main/Main.jsx'
+import Main from './main/Main.jsx'
 
 
 function App() {
---
+++ src/main/Main.jsx
@@ -0,0 +1,24 @@
+import "./main.css";
+import mainPhoto from "./../img/main_photo.png";
+import nextIcon from "./../img/next_icon.svg";
+
+function Main() {
+  return (
+    <section className="main">
+      {/* <img src={mainPhoto} className="main_photo"></img> */}
+
+      {/* <div className="purple_form"> */}
+        {/* <p>some text describing our bestseller</p> */}
+        {/* <button className="purple_button"> */}
+          {/* <h1>add to the cart</h1> */}
+        {/* </button> */}
+      {/* </div> */}
+
+      {/* <button className="main__next_button">
+        <img src={nextIcon}></img>
+      </button> */}
+    </section>
+  );
+}
+
+export default Main;
